df614637c8fd5793321d5bf6a41307bddaa35898
pranjalsingla <88027633+pranjalsingla@users.noreply.github.com>
2022-05-08 09:57:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start(){
+document.getElementById("textbox").innerHTML="";
+recognition.start();
+}
+recognition.onresult = function(event) {
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML=Content;
+    console.log(Content);
+    if(Content =="take my selfie"){
+    console.log("taking selfie---");
+    speak();
+    }
+}
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = "Taking your selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    setTimeout(function(){
+        take_selfie();
+        save();
+    },5000);
+    Webcam.attach(camera);
+  }
+
+ camera = document.getElementById("camera");
+ Webcam.set({
+     width: 360,
+     height: 250,
+     image_format: 'png',
+     png_quality: 90
+ });
+
+function take_selfie(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie" src="'+data_uri+'">';
+        
+    });
+
+}
+
+function save(){
+    link= document.getElementById("link");
+    image = document.getElementById("selfie").src;
+    link.href = image;
+    link.click();
+}
